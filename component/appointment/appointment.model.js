42a106910b044c5abd93180e0d6f480fd4083caa
Ronald803 <ronaldblancobalboa@gmail.com>
2026-01-26 01:21:28 -0400

    Add appointment model

---
+++ component/appointment/appointment.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const mySchema = new Schema({
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: [true, "Patient Id is required"],
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+    required: [true, "Doctor Id is required"],
+  },
+  day: {
+    type: String,
+    required: true,
+  },
+  startHour: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+});
+
+const model = mongoose.model("Appointment", mySchema);
+module.exports = model;

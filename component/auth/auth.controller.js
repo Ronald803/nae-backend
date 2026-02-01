7266bc52f91a1f675e088ca331d6d7e10bd9c280
Ronald803 <ronaldblancobalboa@gmail.com>
2026-01-31 20:26:46 -0400

    Add condition to login process

--- component/auth/auth.controller.js
+++ component/auth/auth.controller.js
@@ -3,26 +3,22 @@ const jwt = require("jsonwebtoken");
 const storeDoctor = require("../doctor/doctor.store");
 
 const login = async ({ email, password }) => {
-  if (!email || !password) {
-    throw new Error("Incomplete Data");
-  }
+  if (!email || !password) throw new Error("Incomplete Data");
   //____________________ checking email exists _________________
-  const user = await storeDoctor.getDoctorToAuth({ email });
-  if (user.length < 1) {
-    throw new Error("Wrong information");
-  }
+  const users = await storeDoctor.getDoctorToAuth({ email });
+  if (users.length < 1) throw new Error("Wrong information");
+  const user = users[0];
+  if (user.status != "active") throw new Error("Inactive account");
   //____________________ checking password is correct ___________
-  const validPassword = bcryptjs.compareSync(password, user[0].password);
-  if (!validPassword) {
-    throw new Error("Wrong information");
-  }
-  const payload = { uid: user[0]._id };
+  const validPassword = bcryptjs.compareSync(password, user.password);
+  if (!validPassword) throw new Error("Wrong information");
+  const payload = { uid: user._id };
   const token = jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
     expiresIn: "4h",
   });
   return {
-    name: user[0].name,
-    rol: user[0].rol,
+    name: user.name,
+    rol: user.rol,
     token,
   };
 };
